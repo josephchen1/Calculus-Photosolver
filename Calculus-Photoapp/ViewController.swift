96fbc3045236c7996bb46b1962585bed2d8474ca
Joseph <jchen1@exeter.edu>
2020-03-30 17:46:52 -0700

    Initial Commit

---
+++ Calculus-Photoapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculus-Photoapp
+//
+//  Created by Joseph Chen on 3/30/20.
+//  Copyright © 2020 Joseph Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Calculus-PhotoappTests/Calculus_PhotoappTests.swift
@@ -0,0 +1,34 @@
+//
+//  Calculus_PhotoappTests.swift
+//  Calculus-PhotoappTests
+//
+//  Created by Joseph Chen on 3/30/20.
+//  Copyright © 2020 Joseph Chen. All rights reserved.
+//
+
+import XCTest
+@testable import Calculus_Photoapp
+
+class Calculus_PhotoappTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
